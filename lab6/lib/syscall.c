7e7c8b983bcd41aa04a2ef7243c744f4699c5c41
WenYang-Lai <x3639026@gmail.com>
2018-05-23 15:40:26 +0800

    {Add}[lab6]

---
+++ lab6/lib/syscall.c
@@ -0,0 +1,85 @@
+#include <inc/syscall.h>
+#include <inc/trap.h>
+
+#define SYSCALL_NOARG(name, ret_t) \
+   ret_t name(void) { return syscall((SYS_##name), 0, 0, 0, 0, 0); }
+
+
+static inline int32_t
+syscall(int num, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, uint32_t a5)
+{
+	int32_t ret;
+
+	// Generic system call: pass system call number in AX,
+	// up to five parameters in DX, CX, BX, DI, SI.
+	// Interrupt kernel with T_SYSCALL.
+	//
+	// The "volatile" tells the assembler not to optimize
+	// this instruction away just because we don't use the
+	// return value.
+	//
+	// The last clause tells the assembler that this can
+	// potentially change the condition codes and arbitrary
+	// memory locations.
+
+	asm volatile("int %1\n"
+		: "=a" (ret)
+		: "i" (T_SYSCALL),
+		  "a" (num),
+		  "d" (a1),
+		  "c" (a2),
+		  "b" (a3),
+		  "D" (a4),
+		  "S" (a5)
+		: "cc", "memory");
+
+	return ret;
+}
+
+
+SYSCALL_NOARG(getc, int)
+SYSCALL_NOARG(getcid, int32_t)
+
+void
+puts(const char *s, size_t len)
+{
+	syscall(SYS_puts,(uint32_t)s, len, 0, 0, 0);
+}
+
+/* TODO: Lab 5
+ * Please add interface needed for 
+ * sleep, settextcolor, kill_self, fork, getpid, cls, get_num_free_page,
+ * and get_num_used_page
+ * Those syscall interface are needed by shell.c to implement basic 
+ * function of the shell
+ *
+ * HINT: You can use SYSCALL_NOARG to save your time.
+ */
+
+int32_t get_num_used_page(void){
+	return syscall(SYS_get_num_used_page, 0, 0, 0, 0, 0);
+}
+int32_t cls(void) {
+    return syscall(SYS_cls, 0, 0, 0, 0, 0);
+}
+int32_t get_num_free_page(void) {
+    return syscall(SYS_get_num_free_page, 0, 0, 0, 0, 0);
+}
+unsigned long get_ticks(void) {
+    return syscall(SYS_get_ticks, 0, 0, 0, 0, 0);
+}
+void settextcolor(unsigned char forecolor, unsigned char backcolor) {
+    syscall(SYS_settextcolor, forecolor, backcolor, 0, 0, 0);
+}
+int32_t fork(void) {
+    return syscall(SYS_fork, 0, 0, 0, 0, 0);
+}
+int32_t getpid(void) {
+    return syscall(SYS_getpid, 0, 0, 0, 0, 0);
+}
+void kill_self() {
+    syscall(SYS_kill, 0, 0, 0, 0, 0);
+}
+void sleep(uint32_t ticks) {
+    syscall(SYS_sleep, ticks, 0, 0, 0, 0);
+}
---
+++ lab6/readme.md
@@ -0,0 +1,30 @@
+### nctuOS
+
+A tiny OS that used for course OSDI in National Chiao Tung University, Computer Science Dept.
+
+This OS only supports x86
+
+### Lab 6
+
+In this lab, you will learn about how to make os support symmertric multiprocessing (SMP) and simple scheduling policy for SMP.
+
+**Source Tree**
+
+`kernel/*`: Includes all the file implementation needed by kernel only.
+`lib/*`: Includes libraries that should be in user space.
+`inc/*`: Header files for user.
+`user/*`: Files for user program.
+`boot/*`: Files for booting the kernel.
+
+You can leverage `grep` to find out where to fill up to finish this lab.
+
+`$ grep -R TODO .`
+
+To run this kernel
+
+    $ make
+    $ make qemu
+
+To debug
+
+    $ make debug
